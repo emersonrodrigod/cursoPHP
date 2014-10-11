bdb91cab7433acc80d6c9560c9a209e191e41975
unknown <emersonrodrigod@gmail.com>
2014-10-11 10:57:29 -0300

    Funcçoes

---
+++ aula3/funcoes.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * Funções são blocos reutilizaveis de codigo
+ */
+
+function escreveNome(){
+    echo "Emerson";
+}
+
+escreveNome();
+
+function retornaValor(){
+    return 10;
+}
+
+$numero = retornaValor();
+echo $numero;
+
+function soma($numero1, $numero2, $numero3, $numero4){
+    
+}
