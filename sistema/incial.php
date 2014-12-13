3d1758ceb539bb3532638560177a56853a6da8fd
unknown <emersonrodrigod@gmail.com>
2014-12-13 11:22:05 -0200

    Finalização do sistema

--- sistema/incial.php
+++ sistema/incial.php
@@ -46,3 +46,90 @@ function recuperaSaldo($idConta) {
         R$ <?php echo number_format($total, 2, ',', '.'); ?>
     </h3>
 </div>
+
+<div style="clear: both"></div>
+
+<div class="row-fluid">
+    <div class="span6">
+        <h4>Gastos por Categoria</h4>
+        <hr/>
+        <div id="piechart"></div>
+    </div>
+
+    <div class="span6">
+        <h4>Receitas por Categoria</h4>
+        <hr/>
+        <div id="receitas"></div>
+    </div>
+</div>
+
+<?php
+$sql = "select categoria.nome as categoria, sum(valor) as valor from lancamento inner join categoria on (categoria.id = lancamento.id_categoria) where tipo = 'D' group by categoria.nome";
+$resultado = mysql_query($sql);
+
+$dados = array(array("Categoria", "Valor"));
+$maior = 0;
+$categoria;
+
+while ($linha = mysql_fetch_assoc($resultado)) {
+    if ($maior < $linha['valor']) {
+        $maior = $linha['valor'];
+        $categoria = $linha['categoria'];
+    }
+    array_push($dados, array($linha['categoria'], round($linha['valor'])));
+}
+
+$grafico = json_encode($dados);
+?>
+
+<?php
+$sqlReceita = "select categoria.nome as categoria, sum(valor) as valor from lancamento inner join categoria on (categoria.id = lancamento.id_categoria) where tipo = 'R' group by categoria.nome";
+$resultadoReceita = mysql_query($sqlReceita);
+
+$dadosReceita = array(array("Categoria", "Valor"));
+
+while ($linha = mysql_fetch_assoc($resultadoReceita)) {
+    array_push($dadosReceita, array($linha['categoria'], round($linha['valor'])));
+}
+
+$graficoReceita = json_encode($dadosReceita);
+?>
+
+<div class="alert alert-error">
+    <strong>Cuidado! Você está gastando muito com a categoria <?php echo $categoria; ?></strong>
+</div>
+
+<script type="text/javascript" src="https://www.google.com/jsapi"></script>
+<script type="text/javascript">
+
+    google.load("visualization", "1", {packages: ["corechart"]});
+    google.setOnLoadCallback(drawChart);
+    google.setOnLoadCallback(drawChartR);
+
+    function drawChart() {
+
+        var data = google.visualization.arrayToDataTable(<?php echo $grafico; ?>);
+
+        var options = {
+            title: 'Gastos por categoria'
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+        chart.draw(data, options);
+    }
+
+    function drawChartR() {
+
+        var data = google.visualization.arrayToDataTable(<?php echo $graficoReceita; ?>);
+
+        var options = {
+            title: 'Receitas por categoria'
+        };
+
+        var chartR = new google.visualization.PieChart(document.getElementById('receitas'));
+
+        chartR.draw(data, options);
+    }
+
+</script>
